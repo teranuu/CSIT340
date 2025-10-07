9af2d5dd61c963c9deb8a68b80b22b7710ca924e
Denn Cayacap <denncayacap@gmail.com>
2025-10-08 03:04:40 +0800

    (in progress / 08.10.2025) feature/login: styled login component

--- core-threads-app/src/components/LoginComponent/LoginSection.jsx
+++ core-threads-app/src/components/LoginComponent/LoginSection.jsx
@@ -12,6 +12,9 @@ function LoginSection(){
 
             <div className="login-section-content">
 
+                <div className="section-left">
+
+
                     <div className="login-section-title">
                         WELCOME BACK!
                     </div>
@@ -21,26 +24,64 @@ function LoginSection(){
                     </div>
 
                     <div className="input-username">
-
                         <input placeholder="Username"></input>
-
                     </div>
 
                     <div className="input-password">
-
                         <input placeholder="Password"></input>
-
-
                     </div>
 
                     <div className="button-sign-in">
-
                         <button>Sign In!</button>
+                    </div>
+
+                    <div className="section-right">
+
+
 
                     </div>
 
+
+
+                </div>
+
+                <div className="section-right">
+
+
+                    <div className="login-section-title">
+                        WELCOME BACK!
+                    </div>
+
+                    <div className="login-section-subtitle">
+                        Enter your credentials to log-in!
+                    </div>
+
+                    <div className="input-username">
+                        <input placeholder="Username"></input>
+                    </div>
+
+                    <div className="input-password">
+                        <input placeholder="Password"></input>
+                    </div>
+
+                    <div className="button-sign-in">
+                        <button>Sign In!</button>
+                    </div>
+
+                    <div className="section-right">
+
+
+
+                    </div>
+
+
+
+                </div>
+
                 
             </div>
+
+            
             
 
 
